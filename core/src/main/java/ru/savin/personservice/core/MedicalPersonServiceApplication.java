37fdc4ddb9f07a5a87a53b30c6c053405a637f2e
darkdeaddaset <darkdeaddeset@gmail.com>
2022-10-06 22:00:54 +0300

    Изменение класса

--- core/src/main/java/ru/savin/personservice/core/MedicalPersonServiceApplication.java
+++ core/src/main/java/ru/savin/personservice/core/MedicalPersonServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.savin.core;
+package ru.savin.personservice.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class App {
+public class MedicalPersonServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(MedicalPersonServiceApplication.class, args);
     }
 }
---
+++ core/src/test/java/ru/savin/personservice/core/MedicalPersonServiceTest.java
@@ -0,0 +1,13 @@
+package ru.savin.personservice.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MedicalPersonServiceTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
